01004b33c9b6cb1a7dabbbbacbb759b3b21ef49e
Bismarck Oliveira <bismarckoliver@hotmail.com>
2022-09-24 18:33:51 -0300

    finished chapter

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,26 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOneOrFail({ id: user_id }, { relations: ["games"] })
+    return user
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+    select * from users
+    order by first_name asc
+    `);
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("" +
+      "SELECT * " +
+      "FROM users " +
+      "WHERE first_name ILIKE LOWER($1) AND last_name ILIKE LOWER($2)",
+      [first_name, last_name])
+
   }
 }
